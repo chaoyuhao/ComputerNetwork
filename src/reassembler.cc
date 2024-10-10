72828d3f42710efec7c7b8519a11163269b8df41
cyh <221240013@smail.nju.edu.cn>
2024-10-10 19:59:02 +0800

    Lab1 beautify

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -28,7 +28,6 @@ void Reassembler::check_buffer(uint64_t last_index) {
     return;
   }
   uint64_t len = it->len - (last_index - it->first_index);
-  //cout << "buffer push " << len << ' ' << it->buffer.substr(last_index - it->first_index, len) << '\n';
   output_.writer().push(it->buffer.substr(last_index - it->first_index, len));
   expecting_syn += len;
   check_close(it->is_last_substring);
@@ -42,7 +41,6 @@ Reassembler::Segment Reassembler::merge_lastseg(Segment new_segment) {
 
   uint64_t last_idx = new_segment.first_index + new_segment.len;
   uint64_t prev_last_idx = prev->first_index + prev->len;
-  //cout << "debug:" << new_segment.buffer << ' ' << prev_last_idx << ' ' << new_segment.first_index << ' ' << new_segment.len << ' ' << first_unaccept_idx() <<  '\n';
   if(prev_last_idx < last_idx && prev_last_idx >= new_segment.first_index){
     uint64_t offset = prev_last_idx - new_segment.first_index;
     new_segment.buffer = prev->buffer + new_segment.buffer.substr(offset);
@@ -58,7 +56,6 @@ Reassembler::Segment Reassembler::merge_lastseg(Segment new_segment) {
     new_segment = *prev;
     segments_.erase(prev);
   }
-  // prev_last_idx < new_segment.first_index
   return new_segment;
 }
 
@@ -71,7 +68,6 @@ Reassembler::Segment Reassembler::merge_nextseg(Segment new_segment) {
   while(it != segments_.end() && it->first_index + it->len <= last_idx) {
     new_segment.is_last_substring = new_segment.is_last_substring || it->is_last_substring;
     it = segments_.erase(it);
-    ////cout << "merge next erase" << it->first_index << " " << it->buffer << '\n';
   }
   // 合并最后一块
   if(it != segments_.end() && it->first_index <= last_idx) {
@@ -87,24 +83,21 @@ Reassembler::Segment Reassembler::merge_nextseg(Segment new_segment) {
 
 void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring )
 {
-  // Your code here.
-  //cout << "expecting_syn " << expecting_syn << '\n';
-  if(first_index == expecting_syn) { // push1
+
+  if(first_index == expecting_syn) {
+    
     uint64_t write_len = min(output_.writer().available_capacity(), data.length());
     uint64_t last_index = expecting_syn + write_len;
-    //cout << "push1 " << first_index << ' ' << data << " len->" << write_len <<" args->" << data.substr(0, write_len) <<'\n';
     output_.writer().push(data.substr(0, write_len));
     expecting_syn = last_index;
     check_buffer(last_index);
     check_close(is_last_substring);
 
-  }else if(first_index >= first_unaccept_idx()) { // push2
-    //cout << "push2 " << first_index << ' ' <<data << '\n';
+  }else if(first_index >= first_unaccept_idx()) {
 
     check_close(is_last_substring);
 
-  }else if(first_index < expecting_syn){ // push3
-    //cout << "push3 " <<first_index << ' ' << data << '\n';
+  }else if(first_index < expecting_syn){
 
     if(first_index + data.length() < expecting_syn) {
       check_close(is_last_substring);
@@ -113,15 +106,12 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
 
     uint64_t write_len = min(data.length() - (expecting_syn - first_index), output_.writer().available_capacity());
     uint64_t last_index = expecting_syn + write_len;
-    //cout << "push3 real " << expecting_syn << ' ' << data.substr(expecting_syn - first_index, write_len) << '\n';
     output_.writer().push(data.substr(expecting_syn - first_index, write_len));
     expecting_syn = last_index;
     check_buffer(last_index);
     check_close(is_last_substring);
 
   }else { // first_index > expecting_syn && first_index < expecting_syn + capacity_
-  
-    //cout << "push4 " << first_index << ' ' << data << '\n';
     // push the dataflow into the buffer of Reassembler
     uint64_t len = first_unaccept_idx() - first_index;
     if(len < data.length()) is_last_substring = false;
@@ -135,7 +125,6 @@ uint64_t Reassembler::bytes_pending() const
 {
   uint64_t cnt = 0;
   for(const auto& it : segments_) {
-    ////cout << "segment:" << it.first_index << " [" <<  it.buffer << "] \n";
     cnt += it.len;
   }
   return cnt;
